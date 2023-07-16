d4cadd7862915262477a0ea01fad2c99200411ca
mendezleoa <92312161+mendezleoa@users.noreply.github.com>
2023-07-16 12:49:22 -0400

    Formulario con guardado de localStorage

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
-const tarjetas = document.getElementById("tarjetas")
+const fichas = document.getElementById("fichas");
+const form = document.querySelector("#form");
 
-crearTarjetas()
+document.getElementById("agregar").addEventListener('click', () => {
+  document.getElementById("formulario").classList.toggle("activo");
+})
 
-function crearTarjetas() {
-  let datos = localStorage.getItem('datos')
+let arraydatos = [];
+
+const CrearItem = (nombre, cedula, departamento) => {
+  if ((nombre.length > 18 || nombre.length < 4) || (cedula.length > 9 || cedula.length < 5) || (departamento.length > 32 || departamento.length < 4)) {
+    alert("Información invalida.")
+  } else {
+    let item = {
+      nombre: nombre,
+      cedula: cedula,
+      departamento: departamento
+    }
+    arraydatos.push(item)
+    localStorage.setItem('datos', JSON.stringify(arraydatos));
+  }
+}
+
+// crearFichas();
+
+function crearFichas() {
+  let datos = localStorage.getItem('datos');
   if (!datos) {
-    tarjetas.innerHTML = `<div>
+    fichas.innerHTML = `<div class="mensaje">
       <h3>No hay datos</h3>
     </div>`
   } else {
-    tarjetas.innerHTML = `<div>
+    fichas.innerHTML = `<div>
       <h3>Si hay datos</h3>
-    </div>`
+    </div>`;
+    datos.forEach(dato => {
+      fichas.innerHTML += `<div>
+      <h3>${dato}</h3>
+    </div>`;
+    });
   }
-}
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let nombre = document.getElementById("nombre").value;
+  let cedula = document.getElementById("cedula").value;
+  let departamento = document.getElementById("departamento").value;
+
+  CrearItem(nombre, cedula, departamento);
+})
